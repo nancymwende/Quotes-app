fcd986e5035af904546f575adacfddd3e7d3edc8
bkee9 <nancy.mwende@student.moringaschool.com>
2022-04-12 08:15:53 +0300

    added the toggle effect

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,11 +8,41 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
-    { id: 1, name: 'Whoever is happy will make others happy too', author: "Nancy" },
-    { id: 1, name: 'The purpose of our lives is to be happy', author: "Nelson Mandela" },
+    new Quote(
+      1,
+      'Nancy Mwende',
+      'Whoever is happy will make others happy too',
+      'Nancy',
+      new Date(2022, 3, 10)
+      ,0,0
+    ),
+    new Quote(
+      2,
+      'Nancy Mwende',
+      'The purpose of our lives is to be happy',
+      'Nelson Mandela',
+      new Date(2022, 3, 10)
+      ,0,0
+    ),
   ];
+
+  toggleDetails(index: any) {
+    this.quotes[index].showAuthor = !this.quotes[index].showAuthor;
+  }
+  completeQuote(isComplete: any, index: any) {
+    if (isComplete) {
+      this.quotes.splice(index, 1);
+    }
+  }
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  addNewQuote(quote: Quote) {
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength + 1;
+    quote.Date = new Date(quote.Date);
+    this.quotes.push(quote);
+  }
 }
