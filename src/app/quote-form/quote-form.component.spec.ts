edb461d2c8ce81dd6fd9057f012d11cce76b6353
bkee9 <nancy.mwende@student.moringaschool.com>
2022-04-12 09:17:36 +0300

    this commit will modify quote-form component.ts

--- src/app/quote-form/quote-form.component.spec.ts
+++ src/app/quote-form/quote-form.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { QuoteFormComponent } from './quote-form.component';
 
-describe('QuoteFormComponent', () => {
+describe('', () => {
   let component: QuoteFormComponent;
   let fixture: ComponentFixture<QuoteFormComponent>;
 
